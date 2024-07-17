72fa24f32fa23aee092b6f5bddf4bfb56593e4aa
Thenmozhi Rathna <145723772+then-mozhi-rathna@users.noreply.github.com>
2024-07-17 22:22:27 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,25 @@
+const myResume = [
+    "name":"Thenmozhi Rathna.J",
+    "email":"thenmozhirathna795@gmail.com",
+    "phone":7411544253,
+    "degree":"MBA"
+];
+
+ //For In loop
+
+for (let x in myResume) {
+    console.log(x);
+  }
+// for of
+  for (let x of myResume) {
+    console.log(x);
+  }
+
+  // for each
+  myResume.forEach((element) => {
+    console.log(element);
+  });
+  
+  
+
+  
